55daf850cc38684732f8d0d3b6251c93058632bf
Zhiyuan Dai <daizhiyuan@phytium.com.cn>
2022-06-17 10:00:50 +0800

    kernel.c: improve whitespace
    Signed-off-by: Zhiyuan Dai <daizhiyuan@phytium.com.cn>

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -14,17 +14,17 @@
 
 void kernel_main(uint64_t dtb_ptr32, uint64_t x1, uint64_t x2, uint64_t x3)
 {
-	/* Declare as unused */
+    /* Declare as unused */
     dtb_ptr32 = dtb_ptr32;
     x1 = x1;
     x2 = x2;
     x3 = x3;
 
-	/* mini UART */
-	uart_init();
+    /* mini UART */
+    uart_init();
 
-	/* Print Initial Starting Message to Serial Console */
-	printk("\n\n");
+    /* Print Initial Starting Message to Serial Console */
+    printk("\n\n");
     printk("        __           __   __                  \n");
     printk(".-----.|  |--.--.--.|  |_|__|.--.--.--------. \n");
     printk("|  _  ||     |  |  ||   _|  ||  |  |        | \n");
@@ -33,30 +33,30 @@ void kernel_main(uint64_t dtb_ptr32, uint64_t x1, uint64_t x2, uint64_t x3)
 
 	printk("\nPhytium Bare-metal system initializing...   \n");
 
-	/* Board Info */
-	printk("\n\tBoard: Raspberry Pi 4\n");
+    /* Board Info */
+    printk("\n\tBoard: Raspberry Pi 4\n");
 
-	/* Arch Info */
-	printk("\tArch: aarch64\n");
-	printk("\n");
+    /* Arch Info */
+    printk("\tArch: aarch64\n");
+    printk("\n");
 
-	/* Exception Levels */
+    /* Exception Levels */
 	int el = get_el();
-	printk("\n----- Exception level: EL%d -----\n", el);
+    printk("\n----- Exception level: EL%d -----\n", el);
 
-	/* Interrupts */
-	printk("Initializing IRQs...");
-	irq_vector_init();
-	printk("Done\n");
+    /* Interrupts */
+    printk("Initializing IRQs...");
+    irq_vector_init();
+    printk("Done\n");
 
-	printk("Enabling IRQ controllers...");
-	enable_interrupt_controller();
-	printk("Done\n");
+    printk("Enabling IRQ controllers...");
+    enable_interrupt_controller();
+    printk("Done\n");
 
-	printk("Enabling IRQs...");
-	irq_enable();
-	printk("Done\n");
+    printk("Enabling IRQs...");
+    irq_enable();
+    printk("Done\n");
 
-	/* Console */
-	console();
+    /* Console */
+    console();
 }
